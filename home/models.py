5791155e6e8a60429301000b296bac06de66f81d
Maya Group <bestangleon@gmail.com>
2021-01-19 16:21:46 +0530

    contact page connect with Contact Models

--- home/models.py
+++ home/models.py
@@ -8,4 +8,8 @@ class Contact(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(max_length=100)
     phone = models.CharField(max_length=15)
-    content = models.TextField(max_length=10000, blank=True)
+    message = models.TextField(max_length=10000, blank=True)
+
+    def __str__(self):
+        return 'Message from' + self.name
+    
--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, HttpResponse
+from home.models import Contact
 
 # Create your views here.
 def home(request):
@@ -8,4 +9,12 @@ def about(request):
     return render(request, 'home/about.html')
 
 def contact(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        message = request.POST['msg']
+        contact = Contact(name=name, email=email, phone=phone, message=message)
+        print(contact)
+        contact.save()
     return render(request, 'home/contact.html')
