d0b312dcb411b7a7ea69a102dc1a4aa6c46fb8b4
Maya Group <bestangleon@gmail.com>
2021-01-19 15:51:36 +0530

    Make A contact model and register app

--- home/models.py
+++ home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Contact(models.Model):
+    Sno = models.AutoField(primary_key=True)
+    time = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    phone = models.CharField(max_length=15)
+    content = models.TextField(max_length=10000, blank=True)
