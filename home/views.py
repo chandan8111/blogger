6907ac13cd779d952264f796880a0d5838e7bbd8
Maya Group <bestangleon@gmail.com>
2021-01-25 14:32:46 +0530

    Add django message framework with bootstrap message components

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse
 from home.models import Contact
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
@@ -14,7 +15,10 @@ def contact(request):
         email = request.POST['email']
         phone = request.POST['phone']
         message = request.POST['msg']
-        contact = Contact(name=name, email=email, phone=phone, message=message)
-        print(contact)
-        contact.save()
+        if len(name) < 3 or len(email) < 5 or len(phone) < 7 or  len(message) < 2 :
+            messages.error(request, "Please fill the form correctly!")
+        else:
+            contact = Contact(name=name, email=email, phone=phone, message=message)
+            contact.save()
+            messages.success(request, "Your message has been sucessfuly sent!")
     return render(request, 'home/contact.html')
