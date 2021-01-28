7b076f0955b8e896472d7d3d61b2ab00908af468
Maya Group <bestangleon@gmail.com>
2021-01-28 12:32:03 +0530

    Created blogpost page

--- blog/views.py
+++ blog/views.py
@@ -9,4 +9,6 @@ def blogHome(request):
     
 
 def blogPost(request, slug):
-    return render(request, 'blog/blogpost.html')
+    post = Post.objects.filter(slug=slug).first()
+    context = {"post": post}
+    return render(request, 'blog/blogpost.html', context)
