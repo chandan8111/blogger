0bfded900f03fcf3fa9497a611dbf2e2b4942000
Maya Group <bestangleon@gmail.com>
2021-01-28 12:16:54 +0530

    Created model to store blog post and show in blog page

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    Sno = models.AutoField(primary_key=True)
+    time = models.DateTimeField()
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    slug = models.CharField(max_length=200, unique=True)
+    content = models.TextField()
+    
+
+    def __str__(self):
+        return self.title + " by " + self.author
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, HttpResponse
+from blog.models import Post
 
 # Create your views here.
 def blogHome(request):
-    return render(request, 'blog/blog.html')
+    allpost = Post.objects.all()
+    context = {'allpost': allpost}
+    return render(request, 'blog/blog.html', context)
     
 
 def blogPost(request, slug):
