b3299aadecd818fa0ffe0337da6b7a95ae6eaf1b
BestAngle <bestangleon@gmail.com>
2021-06-03 09:47:23 +0530

    Displaying replies below corresponding comment

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponse, redirect
 from blog.models import Post, BlogComment
 from django.contrib import messages
+from blog.templatetags import extras
 
 # Create your views here.
 def blogHome(request):
@@ -10,8 +11,15 @@ def blogHome(request):
     
 def blogPost(request, slug):
     post = Post.objects.filter(slug=slug).first()
-    comments = BlogComment.objects.filter(post=post)
-    context = {"post": post, "comments":comments, 'user':request.user}
+    comments = BlogComment.objects.filter(post=post, parent=None)
+    replies = BlogComment.objects.filter(post=post).exclude(parent=None)
+    repDict = {}
+    for reply in replies:
+        if reply.parent.sno not in repDict.keys():
+            repDict[reply.parent.sno] = [reply]
+        else:
+            repDict[reply.parent.sno].append(reply)
+    context = {"post": post, "comments":comments, 'user':request.user, 'replies':repDict}
     return render(request, 'blog/blogpost.html', context)
 
 def postComment(request):
